bd57a54de6d89daa2b99548770c9d88d0e02ac8e
Asta Knudsen <asta.strachottak@gmail.com>
2025-05-07 11:39:48 +0200

    added tests

---
+++ Player/src/test/java/dk/sdu/cbse/playersystem/PlayerControlTest.java
@@ -0,0 +1,23 @@
+package dk.sdu.cbse.playersystem;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerControlTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void process() {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+}
